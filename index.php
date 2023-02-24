30511ff9bd83af479ad87358e1a354834fc41036
matheusbortolottibruno <matheus_bortolotti@yahoo.com>
2023-02-23 22:23:54 -0300

    salvar1

--- index.php
+++ index.php
@@ -1,36 +1,223 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/logan.png" type="image/x-icon">
     <title>Concessionária</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Concessionária</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contato</a>
-                    </li>
 
+<body>
+    <header>
+        <img src="" alt="">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="img/autoesporte.png" alt="" width="150px" height="75px"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                </div>
+
+
+
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PRODUTOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CADASTRO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTATO</a>
+                    </li>
                 </ul>
+    </header>
+
+    <main>
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/carro1.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/carro2.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/carro3.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/carro4.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/carro5.png" class="d-block w-100" alt="...">
+                </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-    </nav>
+
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+
+                    <div class="card" style="width: 20rem;">
+                        <img src="https://www.ecompletocdn.com.br/i/fp/2383/1130645_151_1659731966.png" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title text-success">Arranjo de flores</h5>
+                            <p class="card-text">No nosso site você encontra os melhores arranjo de flores.</p>
+                            <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 20rem;">
+                        <img src="img/produtos01.png" class="card-img-top" alt="Aqui vai uma foto">
+                        <div class="card-body">
+                            <h5 class="card-title text-success">Arranjo de flores</h5>
+                            <p class="card-text">No nosso site você encontra os melhores arranjo de flores.</p>
+                            <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 20rem;">
+                        <img src="https://cf.shopee.com.br/file/sg-11134201-22100-u76obp6v6uiv4f_tn" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title text-success">Arranjo de flores</h5>
+                            <p class="card-text">No nosso site você encontra os melhores arranjo de flores.</p>
+                            <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+    </main>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+
+                <div class="card" style="width: 20rem;">
+                    <img src="img/produtos6.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Arranjo de flores</h5>
+                        <p class="card-text">No nosso site você encontra os melhores arranjo de flores.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="card" style="width: 20rem;">
+                    <img src="img/produtos7.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Arranjo de flores</h5>
+                        <p class="card-text">No nosso site você encontra os melhores arranjo de flores.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="card" style="width: 20rem;">
+                    <img src="https://www.ecompletocdn.com.br/i/fp/14/778152_151_1660596112.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Arranjo de flores</h5>
+                        <p class="card-text">No nosso site você encontra os melhores arranjo de flores.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="col">
+        <div class="container mt-5">
+        <form class="row g-3 needs-validation" novalidate>
+            <div class="col-md-4">
+                <label for="validationCustom01" class="form-label">NOME</label>
+                <input type="text" class="form-control" id="validationCustom01" value="Seu Nome" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+            <div class="col-md-4">
+                <label for="validationCustom02" class="form-label">CIDADE</label>
+                <input type="text" class="form-control" id="validationCustom02" value="Sua Cidade" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+            <div class="col-md-4">
+                <label for="validationCustomUsername" class="form-label">E-mail</label>
+                <div class="input-group has-validation">
+                    <span class="input-group-text" id="inputGroupPrepend">@</span>
+                    <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+                    <div class="invalid-feedback">
+                        Please choose a username.
+                    </div>
+                </div>
+            </div>
+           
+          
+           
+          
+            <div class="col-12">
+                <button class="btn btn-primary" type="submit">Cadastrar</button>
+            </div>
+            </div>
+            </div>
+        </form> 
+
+
+
+
+        <div class="col">
+                <div class="row">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.3286631923656!2d-47.33357331048597!3d-22.75318109057051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8998174c896b9%3A0x739b7f99b3323574!2sSenac%20Americana!5e0!3m2!1spt-BR!2sbr!4v1677113381555!5m2!1spt-BR!2sbr"
+                        width="600%" height="400" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </main>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
+
 </html>
-luxurystate
